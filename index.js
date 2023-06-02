40832f30fc21af3733ed7a50212f81f4fb36c0bf
Kavy4916 <112536268+Kavy4916@users.noreply.github.com>
2023-06-02 20:04:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for(var i =0; i<numberOfDrumButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        animation(this.innerHTML);
+        checker(this.innerHTML);
+    })
+}
+document.addEventListener("keydown",function(event){
+        checker(event.key);
+        animation(event.key);
+})
+function checker(key){
+    switch(key)
+    {
+        case 'a':
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break; 
+        case 'd':
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;         
+        case 'f':
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case 'j':
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break; 
+        case 'k':
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case 'l':
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;                                 
+    }
+}
+function animation(currentKey){
+    var animationKey=document.querySelector("."+currentKey);
+    animationKey.classList.add("pressed");
+    setTimeout(function() {
+        animationKey.classList.remove("pressed");
+    },150);
+}
